942bb680e88c99216382eaa847f61ea5668a3bca
Abdulrazaq2002 <razaq.sharaaz@gmail,com>
2024-03-09 11:24:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Header from "./component/hearder";
+import Header from "./component/header"; // Corrected import statement
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import Course from "./pages/course";
@@ -11,23 +11,26 @@ import Css from "./css/css";
 import Js from "./javascript/javascript";
 import Compiler from "./css/csscompile";
 import About from "./pages/about";
+
 export default function App() {
   return (
     <>
       <Router>
         <Header />
         <Routes>
-          <Route path='/w3academy' Component={Home} />
-          <Route path='compiler' Component={Compiler} />
-          <Route path='/course' Component={Course} />
-          <Route path='/html' Component={Html} />
-          <Route path='/internet' exact Component={Internet} />
-          <Route path='/heading' Component={Heading} />
-          <Route path='/para' Component={Para} />
-          <Route path='/form' Component={Form} />
-          <Route path='/css' Component={Css} />
-          <Route path='/js' Component={Js} />
-          <Route path='/about' Component={About} />
+          <Route path='/w3academy' element={<Home />} /> {/* Updated Route */}
+          <Route path='/compiler' element={<Compiler />} />{" "}
+          {/* Updated Route */}
+          <Route path='/course' element={<Course />} /> {/* Updated Route */}
+          <Route path='/html' element={<Html />} /> {/* Updated Route */}
+          <Route path='/internet' element={<Internet />} />{" "}
+          {/* Updated Route */}
+          <Route path='/heading' element={<Heading />} /> {/* Updated Route */}
+          <Route path='/para' element={<Para />} /> {/* Updated Route */}
+          <Route path='/form' element={<Form />} /> {/* Updated Route */}
+          <Route path='/css' element={<Css />} /> {/* Updated Route */}
+          <Route path='/js' element={<Js />} /> {/* Updated Route */}
+          <Route path='/about' element={<About />} /> {/* Updated Route */}
         </Routes>
       </Router>
     </>
